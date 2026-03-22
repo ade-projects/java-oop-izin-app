7a16a144dcd114628f03304598b819d98a8a4156
ade-projects <projectalpha774@gmail.com>
2026-03-22 18:31:03 +0700

    feat(dosen): tambah subclass dosen

---
+++ src/main/java/com/pnc/izin/entity/Dosen.java
@@ -0,0 +1,65 @@
+package com.pnc.izin.entity;
+
+public class Dosen extends User {
+    private String nip;
+    private boolean isDosenWali;
+    private boolean isKoorProdi;
+
+    public Dosen() {
+        super();
+    }
+
+    public Dosen (int id, String nama, String role, String nip, boolean isDosenWali, boolean isKoorProdi) {
+        super(id, nama, role);
+        this.nip = nip;
+        this.isDosenWali = isDosenWali;
+        this.isKoorProdi = isKoorProdi;
+    }
+
+    public String getNip() {
+        return nip;
+    }
+
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    public boolean isDosenWali() {
+        return isDosenWali;
+    }
+
+    public void setDosenWali(boolean isDosenWali) {
+        this.isDosenWali = isDosenWali;
+    }
+
+    public boolean isKoorProdi() {
+        return isKoorProdi;
+    }
+
+    public void setKoorProdi(boolean isKoorProdi) {
+        this.isKoorProdi = isKoorProdi;
+    }
+
+    // --- METHOD BEHAVIOR ---
+    public void approveIzin() {
+        System.out.println("[DOSEN] Mengecek wewenang untuk menyetujui izin...");
+    }
+
+    @Override
+    public void tampilkanProfil() {
+        super.tampilkanProfil();
+        System.out.println("NIP         : " + nip);
+        String teksJabatan = "";
+        if (isDosenWali && isKoorProdi) {
+            teksJabatan = "Dosen Wali sekaligus Koordinator Prodi";
+        } else if (isDosenWali) {
+            teksJabatan = "Dosen Wali";
+        } else if (isKoorProdi) {
+            teksJabatan = "Koordinator Prodi";
+        } else {
+            teksJabatan = "Dosen Pengajar";
+        }
+
+        System.out.println("Jabatan     : " + teksJabatan);
+    }
+}
