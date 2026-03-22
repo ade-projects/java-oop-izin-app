b9d967a956d90519464d58c9de50897c5cef67f3
ade-projects <projectalpha774@gmail.com>
2026-03-22 18:18:59 +0700

    feat(user): tambah superclass user

---
+++ src/main/java/com/pnc/izin/entity/User.java
@@ -0,0 +1,53 @@
+package com.pnc.izin.entity;
+
+public class User {
+    protected int id;
+    protected String nama;
+    protected String role;
+
+    public User() {
+        
+    }
+
+    public User(int id, String nama, String role) {
+        this.id = id;
+        this.nama = nama;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    /**
+     * Method untuk menampilkan profil dasar
+     */
+    public void tampilkanProfil() {
+        System.out.println("===== PROFIL PENGGUNA ====");
+        System.out.println("ID          : " + id);
+        System.out.println("Nama        : " + nama);
+        System.out.println("Role        : " + role);
+    }
+
+    
+}
