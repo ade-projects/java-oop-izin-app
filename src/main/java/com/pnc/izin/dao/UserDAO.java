b6ac6b0ab16e68922419549f54a6e6abd6e8a0dd
ade-projects <projectalpha774@gmail.com>
2026-03-23 06:34:06 +0700

    feat(userDAO): tambah userDAO

---
+++ src/main/java/com/pnc/izin/dao/UserDAO.java
@@ -0,0 +1,65 @@
+package com.pnc.izin.dao;
+
+import com.pnc.izin.config.DatabaseHelper;
+import com.pnc.izin.entity.Mahasiswa;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO {
+
+    /**
+     * Method untuk menyimpan data Mahasiswa baru ke tabel 'user'
+     */
+    public void tambahMahasiswa(Mahasiswa mhs) {
+        String sql = "INSERT INTO user (nama, role, nim, id_dosen_wali, total_jam_alpa) VALUES (?, ?, ?, ?, ?)";
+
+        try (Connection conn = DatabaseHelper.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, mhs.getNama());
+            pstmt.setString(2, mhs.getRole());
+            pstmt.setString(3, mhs.getNim());
+            pstmt.setInt(4, mhs.getIdDosenWali());
+            pstmt.setInt(5, mhs.getTotalJamAlpa());
+            
+            pstmt.executeUpdate();
+            System.out.println("[DB SUCCESS] Data Mahasiswa berhasil disimpan ke database!");
+
+        } catch (SQLException e) {
+            System.out.println("[DB ERROR] Gagal menyimpan data mahasiswa: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Method untuk mencari Mahasiswa berdasarkan NIM
+     */
+    public Mahasiswa getMahasiswaByNim(String nimInput) {
+        String sql = "SELECT * FROM user WHERE role = 'Mahasiswa' AND nim = ?";
+        Mahasiswa mhs = null;
+
+        try (Connection conn = DatabaseHelper.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, nimInput);
+
+            // Mengeksekusi query pencarian dan menampung hasilnya di ResultSet
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    int id = rs.getInt("id");
+                    String nama = rs.getString("nama");
+                    String role = rs.getString("role");
+                    String nim = rs.getString("nim");
+                    int idDosenWali = rs.getInt("id_dosen_wali");
+                    int jamAlpa = rs.getInt("total_jam_alpa");
+                    mhs = new Mahasiswa(id, nama, role, nim, idDosenWali, jamAlpa);
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("[DB ERROR] Gagal mengambil data mahasiswa: " + e.getMessage());
+        }
+
+        return mhs;
+    }
+}
